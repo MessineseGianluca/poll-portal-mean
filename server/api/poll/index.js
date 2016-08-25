75814e1f540f47f766abf57a1a686c47e796f94e
MessineseGianluca <gianluca.messi1@gmail.com>
2016-08-25 17:41:10 +0200

    Complete poll routing

--- server/api/poll/index.js
+++ server/api/poll/index.js
@@ -5,10 +5,28 @@ var controller = require('./poll.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+/* Polls */
+router.get('/', auth.isAuthenticated(), controller.index);
+router.get('/:id', auth.isAuthenticated(), controller.show);
+router.post('/', auth.hasRole('admin'), controller.create);
+router.post('/:id/answer', auth.isAuthenticated(), controller.answerPoll);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.delete('/:id', auth.hasRole('admin'), controller.destroy);
+
+
+/* Questions */
+router.get('/polls/:id/questions', auth.hasRole('admin'), controller.showQuestions);
+router.get('/polls/:id/questions/:id', auth.hasRole('admin'), controller.showSingleQuestion);
+router.post('/polls/:id/questions', auth.hasRole('admin'), controller.createQuestion);
+router.put('/polls/:id/questions/:id', auth.hasRole('admin'), controller.updateQuestion);
+router.delete('/polls/:id/questions/:id', auth.hasRole('admin'), controller.destroyQuestion);
+
+/* Options */
+router.get('/polls/:id/questions/:id/options', auth.hasRole('admin'), controller.showOptions);
+router.get('/polls/:id/questions/:id/options/:id', auth.hasRole('admin'), controller.showSingleQuestion);
+router.post('/polls/:id/questions/:id/options', auth.hasRole('admin'), controller.createQuestion);
+router.put('/polls/:id/questions/:id/options/:id', auth.hasRole('admin'), controller.updateQuestion);
+router.delete('/polls/:id/questions/:id/options/:id', auth.hasRole('admin'), controller.destroyQuestion);
+
 
 module.exports = router;
