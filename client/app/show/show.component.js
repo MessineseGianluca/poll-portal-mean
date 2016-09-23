aaa02e870ea1f904a4052b2fef5c584cf8bd38c5
MessineseGianluca <gianluca.messi1@gmail.com>
2016-09-23 20:15:00 +0200

    complete show poll result module

--- client/app/show/show.component.js
+++ client/app/show/show.component.js
@@ -16,7 +16,37 @@ export class ShowController {
     this.$http.get('/api/polls/' + this.$stateParams.pollId)
       .then(response => {
         this.poll = response.data;
+        var i = 0;
+        var j = 0;
+        //Num of answers to show for each question(type 'a')
+        this.defaultLimit = 5;
+        
+        for(var question of this.poll.questions) {
+          if(question.type == 'b' || question.type == 'c') {
+            for(var answer of question.answers) {
+              //Find the index of the poption related to the answer
+              var optIndex = _.findIndex(
+                this.poll.questions[i].options,
+                { '_id':  answer.optionId });
+              var numOfChoices = answer.counter;
+              var numOfJoins = this.poll.joins;
+              var percentage = numOfChoices * 100 / numOfJoins;
+              percentage = Number(Math.round(percentage+'e1')+'e-1');
+              this.poll.questions[i].options[optIndex].percentage = percentage;
+              j++;
+            }
+          } else {
+            this.poll.questions[i].limit = this.defaultLimit;
+          }
+          i++;
+        }
+        //Set the number of answers to be showed.(question type a)
+        console.log(this.poll);
       });
+  }
+
+  changeLimit(index) {
+    this.poll.questions[index].limit = this.poll.joins;
   }
 }
 
